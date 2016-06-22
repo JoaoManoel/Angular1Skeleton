8448f2195cebc51a13383b75f21ab43d591d40df
Joao Manoel <joaomanoellins@gmail.com>
2016-06-22 17:29:50 -0300

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .config(configBlock)
+    .run(runBlock);
+
+    configBlock.$inject = [];
+
+    runBlock.$inject = [];
+
+    function configBlock() {
+    }
+
+    function runBlock() {
+    }
+
+})();
